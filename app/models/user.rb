66ba0f4c437828583f8790db90b76ddc319c7f8e
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-15 17:37:22 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :password, :email
   has_secure_password
   has_many :tweets
+
+  def slug
+      self.name.gsub(" ", "-").downcase
+    end
 end
