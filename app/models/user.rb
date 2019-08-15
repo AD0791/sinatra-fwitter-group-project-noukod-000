4db9dd29b1997f9ea723d97acb38e274ceff1e12
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-15 17:37:48 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   def slug
       self.name.gsub(" ", "-").downcase
   end
+
+  def self.find_by_slug(slug)
+      self.all.find{ |instance| instance.slug == slug }
+    end
 end
